35e7de3d6a598111b5df0628636ce6b257eff98d
vlelyavin <vlelyavin@gmail.com>
2022-04-29 18:39:21 +0300

    styling

--- script/register.js
+++ script/register.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("form");
+
 const username = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
