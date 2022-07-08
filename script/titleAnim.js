5ec33861bb75607c97c10b12be7ae50069f8e48f
vlelyavin <vlelyavin@gmail.com>
2022-07-08 12:29:16 +0200

    image format fixed, titles of customer pages fixed

--- script/titleAnim.js
+++ script/titleAnim.js
@@ -1,4 +1,9 @@
 const pageTitle = document.querySelector(".page__title");
+window.onload = () => {
+  pageTitle.style.top = `5vh`;
+  pageTitle.style.opacity = `1`;
+};
+
 window.addEventListener("scroll", () => {
   pageTitle.style.transform = `translateY(-${window.pageYOffset / 3}px)`;
   pageTitle.style.opacity = 1 - window.pageYOffset / 300;
